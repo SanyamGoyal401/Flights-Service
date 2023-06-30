8e5dde03237e82743de749b6761bb84e591cc2dc
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-06-30 16:59:36 +0530

    git added complex join for fetching all airports

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
-
+const {Sequelize, Op, where} = require('sequelize');
 const CrudRepository = require("./crud-repository");
-const { Flight } = require("../models");
+const { Flight, Airplane, Airport} = require("../models");
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -14,6 +14,29 @@ class FlightRepository extends CrudRepository {
             const response = await Flight.findAll({
                 where: filter,
                 order: sort,
+                include: [
+                    {
+                        model: Airplane, 
+                        required : true,
+                        as: "airplane_detail",
+                    },
+                    {
+                        model:Airport,
+                        required: true,
+                        as: "departure_airport",
+                        on:{
+                            col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departure_airport.code"))
+                        },                    
+                    },
+                    {
+                        model:Airport,
+                        required: true,
+                        as: "arrival_airport",
+                        on:{
+                            col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrival_airport.code"))
+                        },                    
+                    }
+                ]
             });
             return response;
         }
