21d4125086862354b7bc544db083c7f8efbc8fc2
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-07-02 00:40:16 +0530

    added Seats model

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -16,7 +16,11 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.Flight,{
         foreignKey: 'airplaneId',
         onDelete: 'CASCADE',
-      })
+      });
+      this.hasMany(models.Seat,{
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE',
+      });
     }
   }
   Airplane.init({
